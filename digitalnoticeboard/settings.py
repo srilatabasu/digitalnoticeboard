074a634be929d00d6bc54f924392248c72a06f76
srilatabasu <102825311+srilatabasu@users.noreply.github.com>
2022-04-02 18:51:29 +0530

    Update settings.py

--- digitalnoticeboard/settings.py
+++ digitalnoticeboard/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+'''
 try:
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
@@ -91,7 +91,7 @@ except KeyError:
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+'''
 
 
 # Password validation
